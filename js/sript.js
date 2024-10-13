b557f42a9aae3c6e11d3b201e150134704935300
Your Name <ridoferdian3@gmail.com>
2024-10-13 21:18:11 +0700

    bazar

---
+++ js/sript.js
@@ -0,0 +1,20 @@
+const generateWa = (params) => {
+  switch (params) {
+    case "lupis":
+      location.href =
+        "https://wa.me/+6281335333867?text=bang%20saya%20pesan%20Piscok%20nya";
+      break;
+    case "lanun":
+      location.href =
+        "https://wa.me/+6281335333867?text=bang%20saya%20pesan%20Bendu%20nya";
+      break;
+    case "serabi":
+      location.href =
+        "https://wa.me/+6281335333867?text=bang%20saya%20pesan%20Lemper%20nya";
+      break;
+    case "capcin":
+      location.href =
+        "https://wa.me/+6281335333867?text=bang%20saya%20pesan%20EsSelasih%20nya";
+      break;
+  }
+};
